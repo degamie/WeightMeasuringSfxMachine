12e9fa9dfd22eba56b2a2d93781981ba5fb7b09b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-06 22:16:51 +0530

    Add files via upload

--- src/main/java/com/WeightMeasuringSfxMachine/Main.java
+++ src/main/java/com/WeightMeasuringSfxMachine/Main.java
@@ -1,68 +1,75 @@
 package com.WeightMeasuringSfxMachine;
 
 public class Main {
-     public Integer accuracy=0;
-    public Integer Sfx=0;
-    public  String machine_stmt=null;
-    public Integer Wight_val=0;
-    public String weight_unit=null;
-    public Main(Sfx,machine_stmt,Wight_val,weight_unit){
+    public String col = null;
+
+    public char[] weight_unit = new char[6];
+    weight_unit=
+
+    {
+        "KG", "Gram", "t", "tonne", "oz", "lb"
+        ;}
+
+    public Integer Sfx = 0;
+    public String machine_stmt = null;
+    public Integer Wight_val = 0;
+    public Main(String col,char[] weight_unit,Integer Sfx ,String machine_stmt,Integer Wight_val ){
         this.Sfx=Sfx;
-        this.machine_stmt=machine_stmt;
+        this.col=col;
         this.weight_unit=weight_unit;
-
+        this.Wight_val=Wight_val;
     }
-    public String wieght_accuracy(Integer accuracy){//wieght_accuracy declare
-        //Calculating  Weight Detect's Accuracy Iteratively
-        while(accuracy!=0){
-            if(weight_Detect(weight_unit, Wight_val, machine_stmt)==0){
-                accuracy=0;
 
+
+    public String weight_Detect(weight_unit, Integer Wight_val, String machine_stmt) {//weight_Detect method Declare
+        for (int i = 0; i < weight_unit.length(); i++) {
+            while (Wight_val != 0) {
+                if (Wight_val > 0 && Wight_val == 0 && weight_unit[0] || weight_unit[2] || weight_unit[3] || weight_unit[5] || weight_unit[4])
+                    Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
+                Sfx += 1;
+                machine_stmt = "mid Weight";
+            else
+                if (Wight_val > 5 && Wight_val == 5 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
+                    Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
+                Sfx += 2;
+                machine_stmt = "OverWeight1";
+            else
+                if (Wight_val > 15 && Wight_val == 0 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
+                    Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
+                Sfx += 2;
+                machine_stmt = "OverWeight2";
+            else
+                if (Wight_val > 25 && Wight_val == 25 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
+                    Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
+                Sfx += 3;
+                machine_stmt = "OverWeight3";
+            else
+                if (Wight_val > 50 && Wight_val == 50 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
+                    Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
+                Sfx += 4;
+                machine_stmt = "OverWeight4";
             }
-            else if (weight_Detect(weight_unit, Wight_val, machine_stmt)==5)accuracy=25;
-            else if (weight_Detect(weight_unit, Wight_val, machine_stmt)==15)accuracy=50;
-            else if (weight_Detect(weight_unit, Wight_val, machine_stmt)==25)accuracy=75;
-            else if (weight_Detect(weight_unit, Wight_val, machine_stmt)==50)accuracy=100;
-        }return accuracy;
-    }
-    public String weightMeasure(Wight_val){
-        weight_Detect(weight_unit,Wight_val,machine_stmt)+accuracy;
+        } return machine_stmt + Sfx;
     }
 
+    public String wieght_display(col, weight_unit, Integer Wight_val, String machine_stmt) {//weightdisplay  method Declare
+        for (int i = 0; i < weight_unit.length(); i++) {
+            while (Wight_val != 0) {
+                if (Sfx == 1 && (weight_unit[0] || weight_unit[2] || weight_unit[3] || weight_unit[5] || weight_unit[4]))
+                    weight_Detect(weight_unit, Wight_val, machine_stmt) col = "green";
+                if (Sfx == 1 && (weight_unit[0] || weight_unit[2] || weight_unit[3] || weight_unit[5] || weight_unit[4]))
+                    weight_Detect(weight_unit, Wight_val, machine_stmt) col = "yellow";
+                if (Sfx == 1 && (weight_unit[0] || weight_unit[2] || weight_unit[3] || weight_unit[5] || weight_unit[4]))
+                    weight_Detect(weight_unit, Wight_val, machine_stmt) col = "orange";
+                if (Sfx == 1 && (weight_unit[0] || weight_unit[2] || weight_unit[3] || weight_unit[5] || weight_unit[4]))
+                    weight_Detect(weight_unit, Wight_val, machine_stmt) col = "red";
+            }
+        } return col;//printing Color
+    }
+}
 
-    public String weight_Detect(String weight_unit,Integer Wight_val,String machine_stmt){
-        while(Wight_val!=0) {
-            if (Wight_val > 0 && Wight_val == 0 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
-                Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
-            Sfx += 1;
-            machine_stmt = "mid Weight";
-            else
-            if (Wight_val > 5 && Wight_val == 5 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
-                Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
-            Sfx += 2;
-            machine_stmt = "OverWeight1";
-            else
-            if (Wight_val > 15 && Wight_val == 0 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
-                Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
-            Sfx += 2;
-            machine_stmt = "OverWeight2";
-            else
-            if (Wight_val > 25 && Wight_val == 25 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
-                Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
-            Sfx += 3;
-            machine_stmt = "OverWeight3";
-            else
-            if (Wight_val > 50 && Wight_val == 50 && weight_unit == "KG" || weight_unit == "Gram" || weight_unit == "t" || weight_unit == "tonne" || weight_unit == "oz" || weight_unit == "lb")
-                Wight_val = Integer.valueOf(weight_unit.concat(Wight_val, weight_unit));
-            Sfx += 4;
-            machine_stmt = "OverWeight4";
-        }
 
-    }
-    public String Wight_Display(String machine_stmt,Integer Sfx){
-        weight_Detect(weight_unit,Wight_val,machine_stmt)+weightMeasure(accuracy)+System.out.println(Sfx);
-    }
-    }
+
 //        public static void main(String[] args) {
 
 
