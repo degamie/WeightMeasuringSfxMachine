c78fdb9c9a34a7492a569ced27b05df9a769ce64
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-04 20:17:21 +0530

    Add files via upload

--- src/main/java/com/WeightMeasuringSfxMachine/Main.java
+++ src/main/java/com/WeightMeasuringSfxMachine/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public Main main;
     public View Weight_View;
     public View  getWeight_View(View weight_View){return weight_View;}// WeightView's Fetching
+    public void setWeight_View(View Weight_View){this.Weight_View=Weight_View;}//Biding Weight View in App
     public ArrayList<String>weight_menu=new ArrayList<>();
     public String weight_unit=null;
     public Main(Sfx,machine_stmt,Weight_val,weight_unit){
