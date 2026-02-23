8e0550c18d26fe87ea122f0aba63768baf41794b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-23 06:55:01 +0530

    Add files via upload

--- src/main/java/com/WeightMeasuringSfxMachine/Main.java
+++ src/main/java/com/WeightMeasuringSfxMachine/Main.java
@@ -1,4 +1,4 @@
-//WID(22/2/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1
+//WID(23/2/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1
 
 package com.WeightMeasuringSfxMachine;
 
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 public class Main {
     public Integer WieghtUnit;
+    public void setWieghtUnit(Integer wieghtUnit){this.WieghtUnit=wieghtUnit;}
     public Integer getWeightUnit(Integer wieghtUnit){return WieghtUnit;}
     public Time weighttime=null;
     public void existsByWieghtTime(Time weighttime){
