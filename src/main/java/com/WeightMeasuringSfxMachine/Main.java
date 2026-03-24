d017003d025ccadb398225aa1aa0726f0ff6b5ee
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-24 07:49:17 +0530

    Add files via upload

--- src/main/java/com/WeightMeasuringSfxMachine/Main.java
+++ src/main/java/com/WeightMeasuringSfxMachine/Main.java
@@ -1,4 +1,4 @@
-//WID(23/03/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1.1.1.1.1.1.1.1,1.1.1/1.1.1.1
+//WID(24/03/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1.1.1.1.1.1.1.1,1.1.1/1.1.1.1.1
 
 package com.WeightMeasuringSfxMachine;
 
@@ -13,6 +13,10 @@ import java.util.List;
 public class Main extends Thread{
     public Main main;
     public String updateByWeighttime(String WeightTime){setWeighttime(weighttime)+getWeighttime(weighttime)+1;}//Updating WeightTime in App
+    public void existsByweightMachine(String WeightMachine){
+        if(WeightMachine!=null)getWeightMachine(WeightMachine);
+        else getWeightMachine(null);
+    }
     public String  updateByWeightMagchine(String WeightMachine){
         getWeightMachine()+SetWeightMachine(WeightMachine)+1;
     }
