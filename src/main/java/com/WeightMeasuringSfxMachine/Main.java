67f85866f575f9afae77cf5c766cc2b56621a95b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-08 10:45:43 +0530

    Add files via upload

--- src/main/java/com/WeightMeasuringSfxMachine/Main.java
+++ src/main/java/com/WeightMeasuringSfxMachine/Main.java
@@ -1,4 +1,4 @@
-//WID(07/04/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1.1.1.1.1.1.1.1,1.1.1/1.1.1.1.1.1.1.1..1.1/1.1.1.1/1.1.1.1.1/1/1.1,1.1.1/1  .1/1c.1.1/1CC
+//WID(08/04/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1.1.1.1.1.1.1.1,1.1.1/1.1.1.1.1.1.1.1..1.1/1.1.1.1/1.1.1.1.1/1/1.1,1.1.1/11.1   
 
 package com.WeightMeasuringSfxMachine;
 
@@ -11,6 +11,7 @@ import java.util.List;
 
 
 public class Main extends Thread{
+    public float updateByWeightEdge(float weightedge){getWeightedge(weightedge)+setWeightedge(weightedge)+1;}//updating WeightEdge in App
     public void setWeightedge(float weightedge){this.weightedge=weightedge;}
     public float getWeightedge(float weightedge){return weightedge;}
     public float weightedge;
