54fdf923f12cf467b90597030e0d961748b2b97e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-16 09:01:10 +0530

    Add files via upload

--- src/main/java/com/WeightMeasuringSfxMachine/Main.java
+++ src/main/java/com/WeightMeasuringSfxMachine/Main.java
@@ -1,4 +1,4 @@
-//WID(13/04/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1.1.1.1.1.1.1.1,1.1.1/1.1.1.1.1.1.1.1..1.1/1.1.1.1/1.1.1.1.1/1/1.1,1.1.1/11.1.1.1.1s/1.1
+//WID(16/04/2026)//Sarthak Mittal Aka Degamiesign #1.1.1#1.1.1.1CC.1.1 #1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1/1.1.1.1.1.1.1.1,1.1.1/1.1.1.1.1.1.1.1..1.1/1.1.1.1/1.1.1.1.1/1/1.1,1.1.1/11.1.1.1.1s/1.1.1.1
 
 package com.WeightMeasuringSfxMachine;
 
@@ -11,6 +11,13 @@ import java.util.List;
 
 
 public class    Main extends Thread{
+    private int getweightlight(int weightlight) {return weightlight; }
+    public void existsByWeightlight(int weightlight){if(weightlight!=0)getweightlight(weightlight);else getweightlight(0);}
+
+
+
+
+    public int updateByweightlight(int weightline){getWeightline(weightlight)+setweightlight(weightlight)+1;}
     public void setweightlight(int weightlight){this.weightlight=weightlight;}
     public int weightlight=0;
     public void existsByWeightEdge(float weightedge){getWeightedge(weightedge)+setWeightedge(weightedge)+1;}//Updating WeightEdge in App
